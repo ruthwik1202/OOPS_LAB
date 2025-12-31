0c289aa731bc69b04fbc756b892b93814a755ad8
ruthwik1202 <ruthwikpspk39@gmail.com>
2025-12-31 07:21:48 +0530

    Basics of Java

---
+++ EvenOdd.java
@@ -0,0 +1,10 @@
+class EvenOdd { 
+   public static void main(String[] args) {
+   int num = 143;
+   
+   if (num % 2 == 0)
+      System.out.println("Even Number");
+   else
+      System.out.println("Odd Number");
+   }
+}
