3b1e9dab819ba51945aa2f212466e15ccd6df42e
ruthwik1202 <ruthwikpspk39@gmail.com>
2026-01-02 19:11:44 +0530

    Delete StudentDetails.java

--- StudentDetails.java
+++
@@ -1,7 +0,0 @@
-class StudentDetails {
-   public static void main(String[] args) {
-   System.out.println("Name: Ruthwik"); 
-   System.out.println("Roll NO: 25230"); 
-   System.out.println("Branch: CSE"); 
-   }
-}
