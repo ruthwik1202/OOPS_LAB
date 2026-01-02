00b7748a14ffa12ba36ae04de55f511c2e3f23d9
ruthwik1202 <ruthwikpspk39@gmail.com>
2026-01-02 19:15:20 +0530

    Add files via upload

---
+++ Basics_java/CheckNumber.java
@@ -0,0 +1,13 @@
+class CheckNumber {
+   public static void main(String[] args) {
+   int num = 205;
+   
+if (num > 0)
+    System.out.println("Positive Number");
+
+else if (num < 0)
+    System.out.println("Negative Number");
+else
+    System.out.println("Zero");
+   }
+}
---
+++ Basics_java/SwapNumbers.java
@@ -0,0 +1,12 @@
+class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 5, b = 10, temp;
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
