2f5d7f9867e6653a03538847859521fd7b930622
ruthwik1202 <ruthwikpspk39@gmail.com>
2026-01-03 17:18:03 +0530

    Add files via upload

---
+++ Basics_java/Areaoftriangle.java
@@ -0,0 +1,7 @@
+class Areaoftriangle {
+ public static void main(String[] args) {
+ double base=12, height=28;
+ double area=0.5*base*height;
+ System.out.println("Area of Triangle: " +area);
+ }
+}
