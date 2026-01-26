e616f1ad2be958b3d5e41bca8bfa7bd1f582be83
ruthwik1202 <ruthwikpspk39@gmail.com>
2026-01-26 12:23:45 +0530

    Delete Basics_java/Areaoftriangle.java

--- Basics_java/Areaoftriangle.java
+++
@@ -1,7 +0,0 @@
-class Areaoftriangle {
- public static void main(String[] args) {
- double base=12, height=28;
- double area=0.5*base*height;
- System.out.println("Area of Triangle: " +area);
- }
-}
